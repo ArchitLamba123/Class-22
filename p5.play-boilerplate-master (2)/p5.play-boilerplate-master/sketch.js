85a1d311f94192b9c6ca3ee9568da8f70b84b1a9
ArchitLamba123 <78206869+ArchitLamba123@users.noreply.github.com>
2021-03-06 16:23:39 +0800

    Update sketch.js

--- p5.play-boilerplate-master (2)/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master (2)/p5.play-boilerplate-master/sketch.js
@@ -1,9 +1,35 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  
+  eng=Engine.create();
+  world= eng.world;
+var objectOptions= { 
+  isStatic:true
+}
+  object = Bodies.rectangle(390,400,800,30,objectOptions)
+  World.add(world,object)
+
+  var ballOptions= { 
+    restitution:1
+  }
+ball = Bodies.circle(390,200,30,ballOptions)
+    World.add(world,ball)
+  console.log(ball)
+  console.log(ball.position.x)
+  console.log(ball.position.y)
+ 
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background("yellow");  
+ Engine.update(eng)
+  rectMode(CENTER)
+  rect(object.position.x,  object.position.y, 800, 30)
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,30)
 }
